1cca2ffe54681ab058e34ebdd5128b9cd3ddadb5
Davemiracle01 <davidmiracle514@gmail.com>
2025-09-06 18:24:24 +0300

    Add files via upload

---
+++ settings.js
@@ -0,0 +1,13 @@
+const settings = {
+  packname: 'escanor',
+  author: 'dave_254769279076',
+  botName: "ESCANOR",
+  botOwner: 'dave_254769279076', // Your name
+  ownerNumber: '254769279076', //Set your number here without + symbol.
+  giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
+  commandMode: "public",
+  description: "This is a bot for managing group commands and automating tasks.",
+  version: "1.2.0",
+};
+
+module.exports = settings;
